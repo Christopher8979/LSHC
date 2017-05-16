910efc6d9138c4172ef5659cb48e80406f26008c
Christopher Gosala <cgosala@deloitte.com>
2017-05-16 13:24:53 +0530

    Added validation on the entry page.

--- public/javascripts/entry.js
+++ public/javascripts/entry.js
@@ -1,3 +1,22 @@
 $(document).on('initialize-entry', function() {
-  console.info('Initialized entry js');
+
+  $('#enterGame').on('click', function(e) {
+    var form = $('#credForm');
+    var noOfFiels = $(form).find('input').length;
+    var fieldsFilled = 0;
+
+    $(form).find('input').each(function(index, elem) {
+      if ($(elem).val() && $(elem).val() !== '') {
+        fieldsFilled++;
+      }
+    });
+
+    if (fieldsFilled === noOfFiels) {
+      $(form).removeClass('fill-all');
+      localStorage.setItem('user', $('#name').val());
+      location.href = "/play-game";
+    } else {
+      $(form).addClass('fill-all');
+    }
+  });
 });
