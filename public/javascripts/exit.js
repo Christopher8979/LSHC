9a948d6ffb5f206ddda820f6fa3ccb820fc51d47
Christopher Gosala <cgosala@deloitte.com>
2017-06-07 15:31:26 +0530

    added buttons and their appearance on exit page

--- public/javascripts/exit.js
+++ public/javascripts/exit.js
@@ -1,8 +1,9 @@
 $(document).on('initialize-exit', function() {
   var name = localStorage.getItem('player');
   var time = localStorage.getItem('completedIn');
+  var how = localStorage.getItem('how');
 
-  if (name === null || time === null) {
+  if (name === null || time === null || how === null) {
     location.href = '/';
   } else {
     time = time / 1000;
@@ -10,8 +11,19 @@ $(document).on('initialize-exit', function() {
     var seconds = time % 60;
 
     $('.name').text(name);
+
+    if (how !== 'with-in-time') {
+      seconds = 0;
+    } else if (how !== 'time-out') {
+      $('#restart').remove();
+      $('#quit').addClass('offset-s3');
+    }
+
     $('.min').text(minutes);
     $('.sec').text((seconds).toFixed(2));
+
+
+    localStorage.removeItem('how');
     localStorage.removeItem('player');
     localStorage.removeItem('completedIn');
   }
