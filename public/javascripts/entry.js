61838c6c64037ce6ac69bbc6348b7c92419342fe
Christopher Gosala <cgosala@deloitte.com>
2017-06-11 21:33:57 +0530

    handled validation for text field

--- public/javascripts/entry.js
+++ public/javascripts/entry.js
@@ -19,4 +19,18 @@ $(document).on('initialize-entry', function() {
       $(form).addClass('fill-all');
     }
   });
+
+  $('#credForm input[type="text"]').bind('keydown', function(e) {
+    console.log(e.keyCode);
+    var elem = $(this);
+    var KEYS_TO_OMIT = [32, 37, 39, 8, 9];
+
+    if (KEYS_TO_OMIT.indexOf(e.keyCode) === -1) {
+      if ((e.keyCode > 64 && e.keyCode < 91) || (e.keyCode > 96 && e.keyCode < 123)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
 });
--- public/javascripts/game_partials/_4_events.js
+++ public/javascripts/game_partials/_4_events.js
@@ -1,99 +1,107 @@
 // Drop Tokens
-dropTokens = function (flag) {
-    var tokens = (flag) ? ptokens : ntokens;
-    var tokenIndex = Math.floor(Math.random() * tokens.length - 1) + 1;
-    var token = tokens[tokenIndex];
+dropTokens = function(flag) {
+  var tokens = (flag) ? ptokens : ntokens;
+  var tokenIndex = Math.floor(Math.random() * tokens.length - 1) + 1;
+  var token = tokens[tokenIndex];
 
-    // Check for animation
-    if (token.isAnimating) { return; }
-    token.isAnimating = true;
-    token.notCollectd = true;
+  // Check for animation
+  if (token.isAnimating) {
+    return;
+  }
+  token.isAnimating = true;
+  token.notCollectd = true;
 
-    token.x = Math.floor(Math.random() * w) + 1;
-    token.y = - 200;
-    stage.addChild(token);
-    createjs.Tween.get(token).to({
-        y: w
-    }, speed * 45).call(function () {
-        token.isAnimating = false
-        stage.removeChild(token)
-    })
+  token.x = Math.floor(Math.random() * w) + 1;
+  token.y = -200;
+  stage.addChild(token);
+  createjs.Tween.get(token).to({
+    y: w
+  }, speed * 45).call(function() {
+    token.isAnimating = false
+    stage.removeChild(token)
+  })
 }
 
 // Token Collection Handler
-tokenCollected = function (token, flag) {
-    stage.removeChild(token);
-    if (token.notCollectd) {
-        token.notCollectd = false;
-        score.value = (flag) ? score.value + 10 : score.value - 10;
-        score.ob.text = "SCORE:" + (score.value);
+tokenCollected = function(token, flag) {
+  stage.removeChild(token);
+  if (token.notCollectd) {
+    token.notCollectd = false;
+    score.value = (flag) ? score.value + 10 : score.value - 10;
+    score.ob.text = "SCORE:" + (score.value);
 
-        // Set default sound to quarter the main volume of the game
-        var soundVol = sound.volume * effectVolRatio;
+    // Set default sound to quarter the main volume of the game
+    var soundVol = sound.volume * effectVolRatio;
 
-        if (flag) {
-            // increment token till we have 3 positinve things collected
-            if (tokensCaught<3) {
-                tokensCaught++;
-            }
-            // Fire show hint once 3 tokens are collected
-            if (tokensCaught === 3) {
-                // tokensCaught = 0;
-                $(document).trigger("showhint");
-            }
+    if (flag) {
+      // increment token till we have 3 positinve things collected
+      if (tokensCaught < 3) {
+        tokensCaught++;
+      }
+      // Fire show hint once 3 tokens are collected
+      if (tokensCaught === 3) {
+        // tokensCaught = 0;
+        $(document).trigger("showhint");
+      }
 
-            // Play positive sound
-            createjs.Sound.play("plusSound", {volume:soundVol});
+      // Play positive sound
+      createjs.Sound.play("plusSound", {
+        volume: soundVol
+      });
 
-        } else {
-            // Uncomment below line to decrease number of tokens taken 
-            // When negetive token is taken
-            
-            // tokensCaught--;
+    } else {
+      // Uncomment below line to decrease number of tokens taken
+      // When negetive token is taken
 
-            // Play negative sound
-            createjs.Sound.play("minusSound", {volume:soundVol});
-        }
+      // tokensCaught--;
+
+      // Play negative sound
+      createjs.Sound.play("minusSound", {
+        volume: soundVol
+      });
     }
+  }
 }
 
 // Encountered Ditch
-hitDitch = function (hit) {
-    if(!hitFlags.ditch && hit) {
-        hitFlags.ditch = true;
-        ambulance.gotoAndPlay("hickup");
-        $(document).trigger("hit-ditch");
-    } 
+hitDitch = function(hit) {
+  if (!hitFlags.ditch && hit) {
+    hitFlags.ditch = true;
+    ambulance.gotoAndPlay("hickup");
+    $(document).trigger("hit-ditch");
+  }
 }
 
 function updateTime(t) {
-    time.text = new Date(t).toISOString().substr(11, 8);
+  time.text = new Date(t).toISOString().substr(11, 8);
 }
 
 
 function moveSprite(e) {
+  if (location.pathname === '/play-game') {
     // console.log(e);
     var type = e.type;
     // 39 - right arrow
     // 37 - left arrow
     if (e.keyCode === 39 || e.keyCode === 37 || e.keyCode === 32) {
 
-        if (e.keyCode === 39) {
-            ambulance.move = (type == "keydown") ? "right" : null;
-        }
+      if (e.keyCode === 39) {
+        ambulance.move = (type == "keydown") ? "right" : null;
+      }
 
 
-        if (e.keyCode === 37) {
-            ambulance.move = (type == "keydown") ? "left" : null;
-        }
+      if (e.keyCode === 37) {
+        ambulance.move = (type == "keydown") ? "left" : null;
+      }
 
-        if (e.keyCode === 32) {
-            if(!$('#questions-modal').hasClass('open') && type == "keydown") {
-              $(document).trigger("play-pause")
-            }
+      if (e.keyCode === 32) {
+        if (!$('#questions-modal').hasClass('open') && type == "keydown") {
+          $(document).trigger("play-pause")
         }
+      }
 
     }
+  }
 }
 
 $(window).on('keydown', moveSprite);
@@ -101,51 +109,55 @@ $(window).on('keyup', moveSprite);
 
 // Sound
 // Mute Button
-$("#mute-btn").on("click", function () {
-    if (!createjs.Ticker.getPaused()) {
-        $(this).toggleClass("btn-clicked");
-    }
-    $(document).trigger("toggle-mute");
+$("#mute-btn").on("click", function() {
+  if (!createjs.Ticker.getPaused()) {
+    $(this).toggleClass("btn-clicked");
+  }
+  $(document).trigger("toggle-mute");
 })
 
 // Play Button
-$("#start-btn").on("click", function () {
-    $(document).trigger("play-pause");
+$("#start-btn").on("click", function() {
+  $(document).trigger("play-pause");
 })
 
 // Game events
-$(document).on("hit-ditch", function () {
-    $(document).trigger("play-pause");
-    $(document).trigger('showquestion');
+$(document).on("hit-ditch", function() {
+  $(document).trigger("play-pause");
+  $(document).trigger('showquestion');
 });
 
-$(document).on("play-pause", function () {
-    createjs.Ticker.setPaused(!createjs.Ticker.getPaused());
-    $("#start-btn").toggle();
-    $(document).trigger("toggle-mute");
+$(document).on("play-pause", function() {
+  createjs.Ticker.setPaused(!createjs.Ticker.getPaused());
+  $("#start-btn").toggle();
+  $(document).trigger("toggle-mute");
 });
 
-$(document).on("update-star", function () {
-    star.ob.gotoAndStop(++star.ob.currentFrame % star.ob.numFrames);
+$(document).on("update-star", function() {
+  star.ob.gotoAndStop(++star.ob.currentFrame % star.ob.numFrames);
 });
 
-$(document).on("toggle-mute", function () {
-    var $mute =  $("#mute-btn");
-    if (sound.volume == 0 && !$mute.hasClass("btn-clicked") && !createjs.Ticker.getPaused()) {
-       $mute.removeClass("active");
-        sound.volume = playVol;
-    } else {
-        $("#mute-btn").addClass("active");
-        sound.volume = 0;
-    }
+$(document).on("toggle-mute", function() {
+  var $mute = $("#mute-btn");
+  if (sound.volume == 0 && !$mute.hasClass("btn-clicked") && !createjs.Ticker.getPaused()) {
+    $mute.removeClass("active");
+    sound.volume = playVol;
+  } else {
+    $("#mute-btn").addClass("active");
+    sound.volume = 0;
+  }
 })
 
-$(document).on("plusSound", function () {
-    // Play positive sound
-    createjs.Sound.play("plusSound", {volume:(playVol * effectVolRatio)});
+$(document).on("plusSound", function() {
+  // Play positive sound
+  createjs.Sound.play("plusSound", {
+    volume: (playVol * effectVolRatio)
+  });
 })
 
-$(document).on("minusSound", function () {
-    // Play negative sound
-    createjs.Sound.play("minusSound", {volume:(playVol * effectVolRatio)});
+$(document).on("minusSound", function() {
+  // Play negative sound
+  createjs.Sound.play("minusSound", {
+    volume: (playVol * effectVolRatio)
+  });
 })
