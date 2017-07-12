abe91d200e33d3659b4c628c88fd237096b11349
Christopher Gosala <cgosala@deloitte.com>
2017-07-12 16:14:43 +0530

    fixed the query to get winner

--- services/gameService.js
+++ services/gameService.js
@@ -132,7 +132,7 @@ var GameService = {
   },
   // gets current winner from SFDC.
   getWinner: function(callBack) {
-    var query = "Select Id, Player__r.Name,Final_Score__c From Player_Attempt__c Order BY Final_Score__c, Question_Answer_Ratio__c, Time_Ratio__c, Token_Points__c DESC limit 1";
+    var query = "Select Id, Player__r.Name, Final_Score__c From Player_Attempt__c Order BY Final_Score__c DESC limit 1";
 
     FS.Query(query, function(err, data) {
       if (err) {
