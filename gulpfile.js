3c5cb39e641bd06ed8c4511ab783a6765c166719
Ashwin P Chandran <ashchandran@deloitte.com>
2017-08-04 16:22:51 +0530

    minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 gulp.task('scripts', function () {
     return gulp.src('./public/javascripts/**/_*.js')
@@ -7,10 +9,20 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./public/javascripts/'));
 })
 
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./public/javascripts/*.js'),
+        uglify(),
+        gulp.dest('./public/javascripts/min/')
+    ],
+    cb
+  );
+});
+
 gulp.task('watch', function () {
     // Watch partial js files
-    gulp.watch("./public/javascripts/**/_*.js", ['scripts']);
+    gulp.watch("./public/javascripts/**/_*.js", ['scripts', 'compress']);
 })
 
 // default task
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'compress', 'watch']);
